45da0f23704ed2e907baf32740b7c2907817afab
Oleksii Fedorov <waterlink000@gmail.com>
2015-11-09 03:06:35 +0100

    Upgrade to active_record 0.1.2

--- script/setup-test-db.sh
+++ script/setup-test-db.sh
@@ -9,6 +9,6 @@ PARAMS="-U ${PG_USER:-postgres}"
 psql $PARAMS -c "create database crystal_pg_test"
 psql $PARAMS -c "create user crystal_pg with superuser password 'crystal_pg'"
 
-psql $PARAMS crystal_pg_test -c "drop table if exists people; create table people( id serial primary key, last_name varchar(50), first_name varchar(50), number_of_dependents int )"
+psql $PARAMS crystal_pg_test -c "drop table if exists people; create table people( id serial primary key, last_name varchar(50), first_name varchar(50), number_of_dependents int, special_tax_group bool )"
 psql $PARAMS crystal_pg_test -c "drop table if exists something_else; create table something_else( id serial primary key, name varchar(50) )"
 psql $PARAMS crystal_pg_test -c "drop table if exists posts; create table posts( id serial primary key, title varchar(50), content text, created_at timestamp )"
